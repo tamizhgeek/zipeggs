e49a587c0e59d01306b8ea3aa62416dcd27b9ffe
Stefaan Lippens <stefaan@netlog.com>
2017-09-25 16:54:15 +0200

    Improve code style

--- zipeggs.py
+++ zipeggs.py
@@ -1,4 +1,8 @@
-import logging, os, zc.buildout, sys, shutil
+import os
+import shutil
+
+import zc.buildout
+
 
 class ZipEggs:
     def __init__(self, buildout, name, options):
@@ -8,7 +12,6 @@ class ZipEggs:
         if options['source'] is None:
             raise zc.buildout.UserError('Invalid Source')
 
-
     def zipit(self):
         target_dir = self.options['target']
         if not os.path.exists(target_dir):
@@ -17,10 +20,10 @@ class ZipEggs:
         for entry in os.listdir(source_dir):
             try:
                 source = os.path.join(source_dir, entry)
-                target = "%s/%s" % (target_dir, entry)
+                target = os.path.join(target_dir, entry)
                 print "%s > %s" % (source, target)
                 shutil.make_archive(target, "zip", source)
-                os.rename(target+".zip", target)
+                os.rename(target + ".zip", target)
             except OSError:
                 print "ignore %s" % entry
         return []
